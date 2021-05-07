578fb62e2c3a04576db7936bc118909732601777
Thierry Delamare <t.delamare@laposte.net>
2021-05-07 19:18:24 +0200

    Adds init-git-store-date

--- misc2.sh
+++ misc2.sh
@@ -22,7 +22,19 @@ use-greek-letters () {
     xmodmap -pke | grep dead_greek | ifne -n xmodmap -pk | grep Control_R | awk '{print $1}' |
 	xargs -i echo xmodmap -e '"keycode {} = dead_greek dead_greek dead_greek dead_greek"'; }
 
-dict set import $self awk.sum bc.kmgt find.size use-greek-letters
+inside-git-p () { [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == true ]]; }
+init-git-store-date () {
+    inside-git-p || return;
+    git-store-dates hooks;
+    [[ -f .saved-dates ]] || {
+	git-store-dates;
+	git ci -m 'Uses git-store-dates';
+	git push;
+    }
+}
+dict set import init-git-store-date inside-git-p
+
+dict set import $self awk.sum bc.kmgt find.size use-greek-letters init-git-store-date
 
 main "$@"
 all
