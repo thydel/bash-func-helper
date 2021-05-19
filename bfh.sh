dd8efd3dcb012292b81abbdac720c8216c1f6e70
Thierry Delamare <t.delamare@laposte.net>
2021-05-19 18:48:42 +0200

    Removes useless func

--- bfh.sh
+++ bfh.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 self=$(basename "${BASH_SOURCE[0]}" .sh)
 fail () { unset -v fail; : "${fail:?$@}"; }
@@ -6,7 +6,7 @@ list () { ls ${1:?}/*.sh | xargs basename -s .sh | xargs -i echo source "<($self
 in-git () { git rev-parse 2> /dev/null; }
 repo-name () { git config --get remote.origin.url | xargs basename -s .git; }
 dev-mode () { in-git && [[ $(repo-name) == bash-func-helper ]]; }
-dev-mode && { BFH_LIB=.; bfh () { try "$@"; }; export -f bfh; }
+dev-mode && BFH_LIB=.
 lib=${BFH_LIB:-/usr/local/lib/bfh}
 [[ $# == 0 ]] && { list $lib; exit 0; }
 mod=$lib/${1:?}.sh
