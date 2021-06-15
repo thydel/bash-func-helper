a7bbac4761c3121733a642f93b5c1a497259be35
Thierry Delamare <t.delamare@laposte.net>
2021-06-15 13:48:17 +0200

    Adds core4 and misc3

--- bfh.mk
+++ bfh.mk
@@ -24,6 +24,7 @@ lib := $(base)/lib/$(name)
 bins := $(name)
 libs := boot git-to-md misc upgrade-jessie
 libs += boot2 misc2 core dupli core2 core3 spl-core3
+libs += core4 misc3
 
 installed := $(bins:%=$(bin)/%) $(libs:%=$(lib)/%.sh)
 
