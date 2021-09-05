9ba8d6a3939c272595f0ce3e5101b7e59d701cd2
Thierry Delamare <t.delamare@laposte.net>
2021-09-05 13:07:43 +0200

    Adds fun

--- mini.sh
+++ mini.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-.run () { declare -f ${1:?}; echo "$@"; }
-
 .src.is-func ()  { declare -f ${1:?} > /dev/null; }
 .src.is-param () { declare -p ${1:?} &> /dev/null; }
 .src () { { .src.is-func $1 && declare -f $1; } || { .src.is-param $1 && declare -p $1; }; }
 
+.run () { declare -f ${1:?}; echo "$@"; }
+.fun () { source <(declare -f . | sed 1s/^./${mod:?}.${1:?}/; echo alias $1="$mod.$1 "); }
 .with () { until [[ ${1:?} == -- ]]; do .src $1; shift; done; .run "${@:2}"; }
 
 .funcs () { compgen -A function ${1:?}; }
@@ -15,17 +15,17 @@
 .use.declare-item () { [[ ${1:?} =~ ^[^._]*[._] ]] && .src $1; }
 .use.declare-loop () { .items ${1:?} | while read; do .use.declare-item $REPLY; done; }
 .use () { until [[ ${1:?} == -- ]]; do .use.declare-loop $1; shift; done; echo "${@:2}"; }
-
 .unuse () { unset -f $(funcs ${1:?}.); unset $(vars ${1}_); }
 
 .import () { local i; for i in ${@:2}; do alias $i="$1.$i "; done; }
-.import '' run src with use unuse
+.import '' src run fun with use unuse
 
 .alias.item () { [[ ${1:?} =~ ^[^.]*[.](.*)$ ]] && alias ${BASH_REMATCH[1]}="$1 "; }
 .alias () { for func in $(.funcs $1); do .alias.item $func; done; }
 
 std.as () { user=${1:?} eval "${@:2}"; }
 std.on () { eval "${@:2}" | ssh ${user:+${user}@}${1:?} bash; }
+std.on-with-stdin () { ssh ${user:+${user}@}${1:?} bash -c "'$(${@:2})'"; }
 
 std.fail () { unset -v fail; : "${fail:?$@}"; }
 std.assert () { $@ || std.fail "$@"; }
