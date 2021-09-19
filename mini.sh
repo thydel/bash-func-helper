87a8746aafb7f82a7b31c5a0b3edf31a433665fe
Thierry Delamare <t.delamare@laposte.net>
2021-09-19 20:24:55 +0200

    sync

--- mini.sh
+++ mini.sh
@@ -38,7 +38,7 @@ macro -l meta src.{alias,func{,.{std,short}}}
 macro -e src
 
 src () { { is-func $1 && src.func $1; } || { is-alias $1 && src.alias $1; }; { is-param $1 && declare -p $1; }; }
-src.alias () { local a=${BASH_ALIASES[$1]% }; alias $1; echo alias $a=$a; }
+src.alias () { local a=${BASH_ALIASES[$1]% }; alias $1; [[ ! -v show || $show != short ]] && echo alias $a=$a; }
 src.func () { self; s=${show:-std}; case $s in std|short) :;; *) fail $s;; esac; $self.$s "$@"; }
 src.func.std () { declare -f ${1:?}; }
 src.func.short () {
@@ -74,8 +74,8 @@ run.no-cd () { declare -f ${1:?}; echo -n "$1"; shift; echo "${IFS:0:1}" "${@@Q}
 run.do-cd () { : ${2:?}; local d=$1; declare -f $2; echo -n "(cd $1; $2"; shift 2; echo "${IFS:0:1}" "${@@Q})"; }
 
 macro -l play with.{no,do}-stdin
-with () { self; case ${1:?} in -i) f=stdin; shift;; -*) fail $1;; *) f=no-stdin;; esac; $self.$f "$@"; }
-with.no-stdin () { until [[ ${1:?} == -- ]]; do src $1; shift; done; run "${@:2}"; }
+with () { self; case ${1:?} in -i) f=do-stdin; shift;; -*) fail $1;; *) f=no-stdin;; esac; $self.$f "$@"; }
+with.no-stdin () { until [[ ${1:?} == -- || $# -eq 0 ]]; do src $1; shift; done; echo "${@:2}"; }
 with.do-stdin () { with "$@"; echo -e " <<EOF\n\$(cat)\nEOF"; cat; }
 
 as () { user=${1:?} eval "${@:2}"; }
