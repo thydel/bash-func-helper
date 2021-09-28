d953f53f16417e6276ce17b189991fc423000e7e
Thierry Delamare <t.delamare@laposte.net>
2021-09-28 12:27:35 +0200

    Fixes pdf.fix-some

--- spl-core3.sh
+++ spl-core3.sh
@@ -20,17 +20,21 @@ pdf.fix-some () {
     qpdf --decrypt --no-original-object-ids --qdf "$2" ~/tmp/decrypt.qdf
     # make JSON objects infos from QDF
     qpdf --json ~/tmp/decrypt.qdf | jq > ~/tmp/decrypt.json
-    # SED to replace REF to unwanted objects to REF to unexistant object
-    local sed="'"; sed+='s/ \(.)$/ 0 0 R/'; sed+="'"
-    # JQ to extract REF of object containing STRING and generate SED to edit REF
-    local jq='[path(.. | select(type == "string" and test($match; "i"))) | .[1]] | map("-e '"$sed"'") | join(" ")'
-    # strip uncrypted QDF from object containing STRING
-    eval sed -r $(jq -r --arg match "$1" "$jq" ~/tmp/decrypt.json) ~/tmp/decrypt.qdf > ~/tmp/stripped.qdf
-    # fix QDF after edition
-    fix-qdf ~/tmp/stripped.qdf > ~/tmp/fixed.qdf
-    # make a PDF from stripped QDF removing unreferenced unwanted objects
     local fixed=$(basename "$2" .pdf)-fixed.pdf
-    qpdf ~/tmp/fixed.qdf $fixed; touch -r $2 $fixed
+    if grep -q "$1" ~/tmp/decrypt.json; then
+	# SED to replace REF to unwanted objects to REF to unexistant object
+	local sed="'"; sed+='s/ \(.)$/ 0 0 R/'; sed+="'"
+	# JQ to extract REF of object containing STRING and generate SED to edit REF
+	local jq='[path(.. | select(type == "string" and test($match; "i"))) | .[1]] | map("-e '"$sed"'") | join(" ")'
+	# strip uncrypted QDF from object containing STRING
+	eval sed -r $(jq -r --arg match "$1" "$jq" ~/tmp/decrypt.json) ~/tmp/decrypt.qdf > ~/tmp/stripped.qdf
+	# fix QDF after edition
+	fix-qdf ~/tmp/stripped.qdf > ~/tmp/fixed.qdf
+	# make a PDF from stripped QDF removing unreferenced unwanted objects
+	qpdf ~/tmp/fixed.qdf $fixed; touch -r $2 $fixed
+    else
+	mv ~/tmp/decrypt.qdf $fixed; touch -r $2 $fixed
+    fi
 }
 name pdf.fix-some pdf.encrypted
 
