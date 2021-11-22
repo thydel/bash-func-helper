54a57ee07ad207f024a5173f1fe5b6a0b867f7e0
Thierry Delamare <t.delamare@laposte.net>
2021-11-22 11:09:48 +0100

    Works in rlib2.sh

--- rlib2.sh
+++ rlib2.sh
@@ -3,52 +3,45 @@
 #set -euo pipefail
 shopt -s expand_aliases
 
-lib () { for i in ${@:2}; do BASH_ALIASES[$i]=${1:?}.$i; done; }
-#lib () { for i in ${@:2}; do BASH_ALIASES[$i]=$1.$i; done; }
+lib () { : ${1:?}; unset -v fail; for i in ${@:2}; do [ -v BASH_ALIASES[$i] ] && "${fail:?$1 $i redefined}" || BASH_ALIASES[$i]=$1.$i; done; }
+
+lib lib splitargs
+splitargs () {
+    local i; local -n _a=${1:?} _b=${2:?}; shift 2; for ((i=1; i<=$#; ++i)); do test "${@:$i:1}" == -- && break; done; _a=("${@:1:(($i-1))}"); _b=("${@:(($i+1)):$#}"); }
+
+lib lib item list head use
+item () { : ${1:?}; local a=(${1//./ }) b=${1/#${a[0]}.}; test -v $1 || test -v $1[@] && declare -p $1; declare -f $1; [ -v BASH_ALIASES[$b] ] && alias $b; }
+list () { compgen -A function ${1:?}.; compgen -v $1_; echo $1; }
+head () { for i in 'set -euo pipefail' 'shopt -s expand_aliases'; do echo $i; done; }
+use () { head; local libs cmd l; splitargs libs cmd "$@"; for l in ${libs[@]}; do list $l | while read; do item $REPLY;  done; done; echo ${cmd[@]}; }
+unalias item list head
 
 lib src macro
 macro () { local -n a=BASH_ALIASES; for i; do [ -v a[$i] ] && { local v=${BASH_ALIASES[$i]}; [ "${v: -1}" != " " ] && a[$i]+=" "; }; done; }
 
-lib std splitargs mapargs; macro mapargs
-splitargs () {
-    local i; local -n _a=${1:?} _b=${2:?}; shift 2; for ((i=1; i<=$#; ++i)); do test "${@:$i:1}" == -- && break; done; _a=("${@:1:(($i-1))}"); _b=("${@:(($i+1)):$#}"); }
-#splitargs () {
-#    local i; local -n _a=$1 _b=$2; shift 2; for ((i=1; i<=$#; ++i)); do test "${@:$i:1}" == -- && break; done; _a=("${@:1:(($i-1))}"); _b=("${@:(($i+1)):$#}"); }
-mapargs () { local cmd args; splitargs cmd args "$@"; for i in "${args[@]}"; do "${cmd[@]}" "$i"; done; }
-
-lib src one all loop head use
-#one () { test -v ${2:?} || test -v $2[@] && declare -p $2; declare -f $2; [ -v BASH_ALIASES[${2/#${1:?}.}] ] && alias ${2/#$1.}; }
-one () { : ${1:?}; local a=(${1//./ }) b=${1/#${a[0]}.}; test -v $1 || test -v $1[@] && declare -p $1; declare -f $1; [ -v BASH_ALIASES[$b] ] && alias $b; }
-all () { compgen -A function ${1:?}.; compgen -v $1_; echo $1; }
-#loop () { mapargs one ${1:?} -- $(all $1); }
-loop () { mapargs one -- $(all $1); }
-head () { mapargs echo -- 'set -euo pipefail' 'shopt -s expand_aliases'; }
-use () { head; local libs cmd; splitargs libs cmd "$@"; mapargs loop -- ${libs[@]}; echo ${cmd[@]}; }
-
 lib meta src self; macro src
 src () { mapargs one -- "$@"; }
-#src () { one ${1:?} ${1:?}.${2:?}; }
-#srcs () { mapargs src ${1:?} -- src ${@:2}; }
 self () { ${FUNCNAME[1]}.${1:?} "${@:2}"; }
 
-lib std fail assert self a2v cnt; macro cnt
+lib std fail assert a2v cnt; macro cnt
 fail () { unset -v fail; : "${fail:?${FUNCNAME[1]} $@}"; }
 assert () { $@ || fail "${FUNCNAME[1]} $@"; }
 self () { ${FUNCNAME[1]}.${1:?} "${@:2}"; }
 a2v () { local -n a=$1; assert test ${#a[@]} -gt 0; for i in ${!a[@]}; do declare -g $i="${a[$i]}"; done; "${@:2}"; }
 cnt () { "$@" | wc -l; }
 
-lib std list map pipe
+lib std list map pipe maparg; macro maparg
 list () { for i; do echo "$i"; done; }
 map () { while read; do "$@" $REPLY; done; }
 pipe () { loc a b; splitargs a b "$@"; < <(${a[@]}) ${b[@]}; }
+mapargs () { local cmd args; splitargs cmd args "$@"; for i in "${args[@]}"; do "${cmd[@]}" "$i"; done; }
 
 
 as () { user=${1:?} eval "${@:2}"; }
 on () { eval "${@:2}" | ssh -A ${user:+${user}@}${1:?}.admin2 bash; }
 
 
-lib ft files dirs list unit du-sum info; declare -A ft=(); ft () { self "$@"; }
+lib ft files dirs unit du-sum info; declare -A ft=(); ft () { self "$@"; }
 
 ft.find () { assert test -d ${1:?}/; find $1 "${@:2}"; }
 
@@ -68,5 +61,5 @@ ft.info () { { hostname; ft.find $1/ -type f | wc -l; ft.find $1/ -type d | wc -
 main () { (($#)) && { eval "$@"; exit $?; }; }
 
 main "$@"
-use src std ft
+use lib std ft
 
